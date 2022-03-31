da9d9227e733ef3195dc01c7369aedd02f6a4ed6
Arthur LeClaire <arthur.j.leclaire@gmail.com>
2022-03-30 22:06:23 -0700

    likes

---
+++ weekTwo.html/likes/javaScriptLikes.js
@@ -0,0 +1,19 @@
+
+var NeilLikes = 9
+function functionIncrease1() {
+
+    NeilLikes++
+    document.querySelector(".boxOneLikes").innerText = NeilLikes + " like(s)"
+}
+var NicoleLikes = 12
+function functionIncrease2() {
+
+    NicoleLikes++
+    document.querySelector(".boxTwoLikes").innerText = NicoleLikes + " like(s)"
+}
+var JimLikes = 9
+function functionIncrease() {
+
+    JimLikes++
+    document.querySelector(".boxThreeLikes").innerText = JimLikes + " like(s)"
+}
