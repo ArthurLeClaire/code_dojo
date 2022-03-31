4251ea09a87db37999835cc94ed32e56f5d9406c
Arthur LeClaire <arthur.j.leclaire@gmail.com>
2022-03-30 22:08:14 -0700

    ceiling

---
+++ Algos/ceiling.js
@@ -0,0 +1,44 @@
+// // 1. DEFINE THE FUNCTION // function d6() { // var roll =
+// Math.floor((Math.random() * 6) + 1); // return roll; // } // // 2. CALL THE
+// FUNCTION // var playerRoll = d6(); // console.log("The player rolled: " +
+// playerRoll); var lifesAnswers = [ "It is certain.", "It is decidedly so.",
+// "Without a doubt.", "Yes – definitely.", "You may rely on it.", "As I see it,
+// yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply
+// hazy, try again.", "Ask again later.", "Better not tell you now.", "Cannot
+// predict now.", "Concentrate and ask again.", "Don't count on it.", "My reply is
+// no.", "My sources say no.", "Outlook not so good.", "Very doubtful." ]; // YOUR
+// CODE GOES HERE // 1. DEFINE THE FUNCTION function oracle(array){ var choice =
+// Math.floor(Math.random() * lifesAnswers.length); var newVar =
+// lifesAnswers[choice]; console.log(newVar); } // 2. CALL THE FUNCTION var answer
+// = oracle(lifesAnswe
+
+// <h1
+// 	onmouseenter="mouseEnterHandler(this)"
+// 	onmouseover="console.log(MOUSEMODE)"
+
+
+// for (var i = 0 ; var < array.length; var++) {
+// 	const element = array[var];
+// 	if(pokemon [i].id>99)
+	
+// }
+
+// for (let index = 0; index < array.length; index++) {
+// 	const element = array[index];
+// }
+
+
+// for(var i=0; i<pokémon.length; i++) {
+//     if(pokémon[i].id  % 3 == 0) {
+//         console.log(pokémon[i].name);
+//     }
+// }
+
+
+// for(var i=0; i<pokémon.length; i++) {
+//     if(pokémon[i].id  % 3 = {
+//         console.log(pokémon[i].type);
+//     }
+// }
+
+
--- weekTwo.html/javaScript.js
+++ weekTwo.html/javaScript.js
@@ -1,15 +1,26 @@
-function login()
-{
-    document.querySelector("#loginButton").text = "Signout";
-};
+// function login()
+// {
+//     document.querySelector("#loginBtn").text = "Signout";
+//  };
 
-function likesOne()
-{
-    alert("Ninja was liked");
-};
+// function likesOne()
+// {
+//     alert("Ninja was liked");
+// };
 
-function removeButton() {
-    try {
-        document.querySelector(".addDef").remove();
-    } catch (error) { }
-};
+// function removeButton() {
+//     try {
+//         document.querySelector(".addDef").remove();
+//     } catch (error) { }
+// };
+console.log("hey")
+
+var JimLikes = 9
+function functionIncrease()
+{
+    console.log("clicked")
+    JimLikes++
+document.querySelector(".boxThreeLikes").innerText= JimLikes+ " likes"
+
+
+}
