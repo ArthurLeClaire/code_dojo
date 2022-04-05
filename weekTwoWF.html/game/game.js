6bdb772dd9da8b9a95ad0858eccf1ccfafb7e4c0
Arthur LeClaire <arthur.j.leclaire@gmail.com>
2022-04-05 16:02:31 -0700

    python week one start

---
+++ weekTwoWF.html/game/game.js
@@ -0,0 +1,47 @@
+function firstFunction()
+{
+    document.img("stonePunk").click();
+}
+
+var increaseCart = 0
+function functionIncrease2() {
+
+    increaseCart++
+    document.querySelector(".cartItems").innerText = increaseCart 
+}
+
+var increaseCart = 0
+function functionIncrease1() {
+
+    increaseCart++
+    document.querySelector(".cartItems").innerText = increaseCart 
+}
+
+var increaseCart = 0
+function functionIncrease() {
+
+    increaseCart++
+    document.querySelector(".cartItems").innerText = increaseCart 
+}
+
+// var toggle = false
+// function changePic() {
+
+//     imgsrc = document.getElementById("stonePunk").src;
+//     if (imgsrc.indexOf(images\stonepunk.png) != 1) {
+//         document.getElementById("stonePunk").src = "cafe-neko.png"
+//     }
+//     else {
+//         document.getElementById("stonePunk").src = "images\stonepunk.png"
+//     }
+// }
+
+
+    // if (toggle === true) {
+    //     document.getElementById('punkStone').src = 'images\cafe-neko.png';
+    // } else {
+    //     document.getElementById('punkStone').src = 'images\stonepunk.png';
+    // }
+    
+    // toggle = !toggle;
+// }
