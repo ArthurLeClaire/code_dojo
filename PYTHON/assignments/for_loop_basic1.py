950640cca8c3ab61830453dd69ba91633dd41b65
Arthur LeClaire <arthur.j.leclaire@gmail.com>
2022-04-06 13:57:18 -0700

    for loop basic update

--- PYTHON/assignments/for_loop_basic1.py
+++ PYTHON/assignments/for_loop_basic1.py
@@ -1,33 +1,33 @@
-for i in range(151):
-    print(i)
-for i in range(5, 1001, 5):
-    print(i)
+# for i in range(151):
+#     print(i)
+# for i in range(5, 1001, 5):
+#     print(i)
 
-for i in range(1, 101):
-    if i % 10 == 0:
-        print("Coding Dojo")
-    elif i % 5 == 0:
-        print("Coding")
-    else:
-        print(i)
+# for i in range(1, 101):
+#     if i % 10 == 0:
+#         print("Coding Dojo")
+#     elif i % 5 == 0:
+#         print("Coding")
+#     else:
+#         print(i)
 
-min = 0
-max = 500000
-Oddtotal = 0
+# min = 0
+# max = 500000
+# Oddtotal = 0
 
-for number in range(min, max+1):
-    if(number % 2 != 0):
-        Oddtotal += number
+# for number in range(min, max+1):
+#     if(number % 2 != 0):
+#         Oddtotal += number
 
-print(f"the sum of odd numbers from {min} to {max} = {Oddtotal}")
+# print(f"the sum of odd numbers from {min} to {max} = {Oddtotal}")
 
-# number = 2018
-# while number > 0:
-#     print(number)
-#     number -= 4
+# # number = 2018
+# # while number > 0:
+# #     print(number)
+# #     number -= 4
 
-for i in range(2018, 0, -4):
-    print(i)
+# for i in range(2018, 0, -4):
+#     print(i)
 
 low = 2
 high = 9
@@ -35,7 +35,7 @@ multi = 3
 
 
 def flex(low, high, multi):
-    for i in range(low, high):
+    for i in range(low, high+1):
         if i % multi == 0:
             print(i)
 
